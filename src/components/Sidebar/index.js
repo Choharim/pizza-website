7e8288a65abeae86b6b2b816872cbfddaefab52a
Choharim <jhl8586@naver.com>
2020-12-14 11:01:06 +0900

    Add sidebar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  CloseIcon,
+  Icon,
+  SidebarContainer,
+  SidebarLink,
+  SidebarMenu,
+  SidebarRoute,
+  SidebarWrap,
+} from "./SidebarElements";
+
+const Sidebar = () => {
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+      <SidebarMenu>
+        <SidebarLink to="/">Pizzas</SidebarLink>
+        <SidebarLink to="/">Desserts</SidebarLink>
+        <SidebarLink to="/">Full Menu</SidebarLink>
+      </SidebarMenu>
+
+      <SidebarWrap>
+        <SidebarRoute to="/">Order Now</SidebarRoute>
+      </SidebarWrap>
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;
