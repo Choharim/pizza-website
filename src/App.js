38c99d66f7256a4750846db7582911f41f8e6048
Choharim <jhl8586@naver.com>
2020-12-14 12:38:51 +0900

    Add products

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Hero from "./components/Hero";
+import Products from "./components/Products";
 import { GlobalStyle } from "./globalStyle";
+import { productData } from "./components/Products/data";
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Hero />
+      <Products heading="choose your favorite" data={productData} />
     </Router>
   );
 }
---
+++ src/components/Products/data.js
@@ -0,0 +1,28 @@
+import product1 from "../../images/product-1.jpg";
+
+export const productData = [
+  {
+    img: product1,
+    alt: "Pizza",
+    name: "Supreme Pizza",
+    desc: "Marinara sauce, basil, italian sausage",
+    price: "$19.99",
+    button: "Add to Cart",
+  },
+  {
+    img: product1,
+    alt: "Pizza",
+    name: "Supreme Pizza",
+    desc: "Marinara sauce, basil, italian sausage",
+    price: "$19.99",
+    button: "Add to Cart",
+  },
+  {
+    img: product1,
+    alt: "Pizza",
+    name: "Supreme Pizza",
+    desc: "Marinara sauce, basil, italian sausage",
+    price: "$19.99",
+    button: "Add to Cart",
+  },
+];
